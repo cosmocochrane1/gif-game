30454e7ab6c36a0c3ac47375b4ea67f3cfe7ef3a
af-inet <davidmhargat@gmail.com>
2017-04-27 16:01:37 -0400

    bullet collision

--- src/game/main.js
+++ src/game/main.js
@@ -12,6 +12,23 @@ function createAsteroids () {
     }
     asteroids.x = 100;
     asteroids.y = 50;
+//to(properties, duration, ease, autoStart, delay, repeat, yoyo)
+    var tween = game.add.tween(asteroids).to(
+      {
+        x: 500
+      },
+      1200,
+      Phaser.Easing.Circular.easeInOut,
+      true,
+      0,
+      5000,
+      true
+    );
+    tween.onLoop.add(move_down, this);
+}
+
+function move_down() {
+    asteroids.y += 32;
 }
 
 const state = {
@@ -21,22 +38,30 @@ const state = {
   preload: function() {
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT
     game.load.image('ship', 'img/ship.gif')
-    game.load.image('asteroid', 'asteroid-icon.png')
-    game.load.image('bullet', 'img/ship.gif')
+    game.load.image('bullet', 'img/bullet.png')
+    game.load.image('asteroid', 'giphy-2.gif')
   },
   create: function() {
 
     asteroids = game.add.group();
     asteroids.enableBody = true;
     asteroids.physicsBodyType = Phaser.Physics.ARCADE;
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.enable(true)
 
+    asteroids = game.add.group()
+    asteroids.enableBody = true
+    asteroids.physicsBodyType = Phaser.Physics.ARCADE
+    
+    bullets = game.add.group()
+
+    // delay bullets
+    cooldown = 0
 
     // create spaceship
     const x = this.world.centerX
     const y = this.world.height - 100
     const ship = game.add.sprite(x, y, 'ship')
-    const b = new Bullet(game, 0, 0, 'bullet')
-    const bullet = game.add.existing(b)
 
 
     ship.anchor.setTo(0.5, 0.5)
@@ -50,17 +75,17 @@ const state = {
     // enable physics
     game.physics.arcade.enable([ship], Phaser.Physics.ARCADE)
 
-    // asteroid = game.add.sprite(0,0, 'asteroid');
+    game.physics.arcade.collide(ship, asteroids)
+
     createAsteroids()
 
     // "body" only exists after you enable physics
     ship.body.collideWorldBounds = true
-
   },
   update: function() {
     const cursors = game.input.keyboard.createCursorKeys()
-    const SPEED = 800
-
+    const SPEED = 1200
+    
     // controls
     if (cursors.left.isDown) {
       game.ship.body.velocity.x = -SPEED
@@ -68,15 +93,36 @@ const state = {
     if (cursors.right.isDown) {
       game.ship.body.velocity.x = SPEED
     }
+    if (cursors.up.isDown) {
 
+      if (game.time.now > cooldown) {
+        // create a new bullet
+        const x = game.ship.x
+        const y = game.ship.y
+        const bullet = new Bullet(game, x, y, 'bullet')
+        game.add.existing(bullet)
+        // add to bullet group
+        bullets.add(bullet)
+        // schedule the next cooldown
+        cooldown = game.time.now + 150
+      }
+    }
     // slow down gradually
-    game.ship.body.velocity.x /= 1.1
+    game.ship.body.velocity.x /= 2
+  },
+  render: function() {
+    //bullets.forEach((b) => {
+    //  game.debug.body(b)
+    //})
+    //asteroids.forEach((b) => {
+    //  game.debug.body(b)
+    //})
   }
 }
 
 const game = new Phaser.Game(
+  1200,
   800,
-  640,
   Phaser.AUTO,
   'game',
   state
